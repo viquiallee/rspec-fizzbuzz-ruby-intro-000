c253cda0018a69d7e396363f92db1ae7ba1abc66
Victoria Alee <victoriaallee@gmail.com>
2020-11-25 04:18:37 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,8 +2,9 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz(int)
-  
-  if int % 3 == 0 
+  if int % 3 == 0 && int % 5 == 0 
+    "FizzBuzz"
+  elsif int % 3 == 0 
     "Fizz"
   elsif int % 5 == 0 
     "Buzz"
