486e9a6182f9ab3881372e03751e316f26a95cc9
Victoria Alee <victoriaallee@gmail.com>
2020-11-25 03:42:06 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,9 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz("int")
-  if "int" /3
+  if "int" == /3
   puts "Fizz"
+  elsif "int" == /5
+  puts "Buzz"
   
 end  
